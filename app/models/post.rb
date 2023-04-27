de33a01ebf5be53032042b05a6a6c9b2b444c253
alex lieberman <alex.lieberman182@gmail.com>
2023-04-27 14:21:13 -0700

    commit tree f069d0bd49789b0f139710fac034b396bf35ea6a

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+
+    validate :must_have_clickbait
+
+    def must_have_clickbait
+        if title != nil && title.exclude?("Won't Believe" || "Secret" || "Top [number]" || "Guess")
+           errors.add(:title, "That is not clickbaity enough!") 
+        end
+    end
 end
